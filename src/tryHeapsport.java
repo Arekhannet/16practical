5953e0c654468b8bc4a5e90615b3d75b4b4b9f18
YourName <4493911@myuwc.ac.za>
2026-03-18 15:06:40 +0200

    main tryheapsort method

--- src/tryHeapsport.java
+++ src/tryHeapsport.java
@@ -1,2 +1,12 @@
-public class tryHeapsport {
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+public class tryHeapsort {
+
+    static final int REPETITIONS = 30;
+
+    public static void main(String[] args) {
+
+    }
 }
