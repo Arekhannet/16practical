c4ffd4d7350b6d3cf1e33d4ae7b613c50f06f880
YourName <4493911@myuwc.ac.za>
2026-03-18 14:45:53 +0200

    heap basic structure and fields

--- src/Heap.java
+++ src/Heap.java
@@ -1,2 +1,13 @@
 public class Heap {
+
+    String[] d;
+    int n;
+    int maxSize;
+
+    // Empty heap constructor
+    public Heap(int maxSize) {
+        this.maxSize = maxSize;
+        d = new String[maxSize + 1];  // d[0] unused
+        n = 0;
+    }
 }
